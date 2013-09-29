0e48f7a706a14190e7f78c5c22e9914cd11990b3
Josh Hill <jamesjoshuahill@gmail.com>
2013-09-29 22:41:30 +0100

    Draft specs for user model

---
+++ spec/user_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe User do
+  it 'should not save the password'
+
+  it 'should save a password digest using BCrypt'
+
+  it 'should authenticate login credentials'
+end
