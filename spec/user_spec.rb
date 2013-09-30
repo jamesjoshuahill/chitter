e0d2e98f962818dbd2122fd8264dff00aa1e2998
Josh Hill <jamesjoshuahill@gmail.com>
2013-09-30 09:42:24 +0100

    Specs for user model

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,9 +1,19 @@
 require 'spec_helper'
 
 describe User do
-  it 'should not save the password'
+  it 'should save a password digest using BCrypt' do
+    expect(BCrypt::Password).to receive(:create).with('password').and_return('password_digest')    
+    user = User.new
+    user.password = 'password'
+    expect(user.password_digest).to eq 'password_digest'
+  end
 
-  it 'should save a password digest using BCrypt'
-
-  it 'should authenticate login credentials'
+  it 'should authenticate login credentials' do
+    user = User.create(name: 'Name',
+                       email: 'Email',
+                       password: 'Password',
+                       password_confirmation: 'Password',
+                       username: 'username')
+    expect(User.authenticate('Email', 'Password')).to eq user
+  end
 end
