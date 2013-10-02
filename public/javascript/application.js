c5c7a2d54ab5b81e9926e1a24a79108658c12333
Josh Hill <jamesjoshuahill@gmail.com>
2013-10-02 11:48:12 +0100

    Change animations for links, flash notice and favourite cheep

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,7 +1,14 @@
-function animateLinks() {
-  $('.cheep').show(1000);
+function hideNotice() {
+  $('#notice').delay(2500).slideUp(500);
+}
+
+function addFavouritesHandler() {
+  $(".star.solid").click(function() {
+    $(this).animate({opacity: 1}, 500);
+  });
 }
 
 $(function() {
-  animateLinks();
+  hideNotice();
+  addFavouritesHandler();
 });
