2276a79feb013c6a52d4a0648568233060fd92df
Josh Hill <jamesjoshuahill@gmail.com>
2013-10-02 12:27:51 +0100

    Draft showing a favourite message via console

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -2,6 +2,15 @@ function hideNotice() {
   $('#notice').delay(2500).slideUp(500);
 }
 
+function showLinkFavouritedNotice(cheep) {
+  var favourited = !!$(cheep).data("favourited");
+  var message = $(cheep).find('.message').text();
+  var log_output = favourited ?
+                message + " was added to favourites" :
+                message + " was removed from favourites";
+  console.log(log_output);
+}
+
 function addFavouritesHandler() {
   $(".star.solid").click(function() {
     var cheep = $(this).parent();
@@ -10,6 +19,8 @@ function addFavouritesHandler() {
     var newOpacity = favourited ? 0 : 1;
     $(cheep).data("favourited", !favourited);
     $(this).animate({opacity: newOpacity}, 1000);
+
+    showLinkFavouritedNotice(cheep)
   });
 }
 
