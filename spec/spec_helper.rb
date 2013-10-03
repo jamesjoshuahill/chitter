83965b7523af7ba1d271e58ef07d574c39881aec
Josh Hill <jamesjoshuahill@gmail.com>
2013-10-03 12:27:08 +0100

    Fix environment for RSpec tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,8 @@ require 'database_cleaner'
 require 'capybara/rspec'
 require './app/chitter'
 
+ENV['RACK_ENV'] = 'test'
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
